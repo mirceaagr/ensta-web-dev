91be7dfff43259523da340c137b9bfaf1367fc9f
mircea_agr <mircea.agrigoroaie@gmail.com>
2020-03-30 10:39:44 +0200

    Add messages boxes

--- 2020/course3/js-continuation/main.js
+++ 2020/course3/js-continuation/main.js
@@ -1,3 +1,7 @@
 // $('body').css("background-color","red");
 // console.dir($);
 
+// errors and success messages
+const errors = document.getElementById('errors');
+const success = document.getElementById('success');
+const form = document.getElementById('registerForm');
