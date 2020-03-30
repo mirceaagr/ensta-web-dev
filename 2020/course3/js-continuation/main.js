12f6f5c10162d1e562744c26149a4d0398d7d093
mircea_agr <mircea.agrigoroaie@gmail.com>
2020-03-30 10:42:44 +0200

    Define boxes and inputs

--- 2020/course3/js-continuation/main.js
+++ 2020/course3/js-continuation/main.js
@@ -5,3 +5,14 @@
 const errors = document.getElementById('errors');
 const success = document.getElementById('success');
 const form = document.getElementById('registerForm');
+
+// inputs
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
+const passConfirm = document.getElementById('passwordConfirm');
+const age = document.getElementById('age');
+const terms = document.getElementById('terms');
+const submitBtn = document.getElementById('submitBtn');
+
+// array with errors
+let errArr = [];
