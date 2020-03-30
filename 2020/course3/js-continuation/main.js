21d3bbd22b8d0d6202bc14098337d0f3a441c41d
mircea_agr <mircea.agrigoroaie@gmail.com>
2020-03-30 11:21:25 +0200

    Form validation completed

--- 2020/course3/js-continuation/main.js
+++ 2020/course3/js-continuation/main.js
@@ -16,8 +16,18 @@ const submitBtn = document.getElementById('submitBtn');
 
 // array with errors
 let errArr = [];
+let formCounter = localStorage.getItem('formCount')
+if(formCounter === null) {
+    formCounter = 0;
+    localStorage.setItem('formCount',0);
+}
 
+
+// function that validates form
 function validateForm(){
+    submitBtn.textContent = "Please wait.."; // we change the text of the button
+    submitBtn.disabled = true; // we disable the button 
+
     if(!isEmailValid(email)) {
         errArr.push("Email is invalid");
     }
@@ -30,8 +40,18 @@ function validateForm(){
         errArr.push("Password confirmation is not valid");
     }
 
+    if(!passwordsMatch(pass, passwordConfirm)) {
+        errArr.push("Passwords do not match");
+    }
+
     if(!isTextInputValid(age)) {
         errArr.push("Age is not valid");
+    } else if (!ageIsApropriate(age)){
+        errArr.push(`Sorry, at the age of ${age.value} I was hunting butterflies..`);
+    }
+
+    if(!isTermsChecked(terms)) {
+        errArr.push("You have to agree with our TOS");
     }
 
     errArr.forEach(err => {
@@ -44,7 +64,23 @@ function validateForm(){
 
     })
 
+    setTimeout(()=>{
+        submitBtn.textContent = "Submit";
+        submitBtn.disabled = false;
+    }, 2000)
 
+}
+
+function isTermsChecked(terms) {
+    return terms.checked;
+}
+
+function passwordsMatch(pass1, pass2) {
+    return pass1.value === pass2.value;
+}
+
+function ageIsApropriate(age) {
+    return age.value > 18;
 }
 
 function isEmpty(input){
@@ -69,15 +105,35 @@ function isTextInputValid(input) {
     return !isNullOrUndefined(input) && !isEmpty(input);
 }
 
+function resetErrors() {
+    errArr = [];
+    while(errors.hasChildNodes()) {
+        errors.removeChild(errors.firstChild);
+    }
+    success.classList.add('invisible');
+}
+
+function checkIfSpam(){
+    if(localStorage.getItem('formCount') > 3) {
+        errArr.push("You are BOT");
+        return true;
+    }
+    return false;
+}
+
 
 
 
 
 form.addEventListener('submit', (event)=>{
+    resetErrors();
+    checkIfSpam();
     validateForm();
     if(!isValid()){
         event.preventDefault(); // we stop the form
     }else{
+        formCounter++;
+        localStorage.setItem('formCount', formCounter)
         success.classList.remove('invisible');
     }
 })
