dc6f73b227172fc4ce7fd443885a6127c592cf0b
mircea_agr <mircea.agrigoroaie@gmail.com>
2020-04-06 09:31:51 +0200

    Add our site on top of angular

--- 2020/course4/pizza/src/app/app-routing.module.ts
+++ 2020/course4/pizza/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PizzasComponent } from './pizzas/pizzas.component';
+import { ContactComponent } from './contact/contact.component';
 
 //{path:... , component: nameofcomponents}
 const routes: Routes = [
-  {path:"", component: HomeComponent}
+  {path:"", component: HomeComponent},
+  {path:"about", component: AboutComponent},
+  {path:"pizzas", component: PizzasComponent},
+  {path:"contact", component: ContactComponent},
+
 ];
 
 @NgModule({
