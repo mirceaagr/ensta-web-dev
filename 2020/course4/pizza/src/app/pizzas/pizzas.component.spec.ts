3854bb853e1df446bdb8085133870eab0c64116c
mircea_agr <mircea.agrigoroaie@gmail.com>
2020-04-06 09:19:14 +0200

    Add components

---
+++ 2020/course4/pizza/src/app/pizzas/pizzas.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzasComponent } from './pizzas.component';
+
+describe('PizzasComponent', () => {
+  let component: PizzasComponent;
+  let fixture: ComponentFixture<PizzasComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PizzasComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PizzasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ 2020/course4/pizza/src/app/pizzas/pizzas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizzas',
+  templateUrl: './pizzas.component.html',
+  styleUrls: ['./pizzas.component.css']
+})
+export class PizzasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
