6fed2fba839c90efe47f2ed10cc6b4f80afe0abc
mirceaagr <mircea.agrigoroaie@gmail.com>
2021-04-08 09:47:00 +0200

    Strings

---
+++ 2021/course3/js/2.js
@@ -0,0 +1,9 @@
+// let lorem = "lorem ipsum dolor sit a met";
+// console.log(lorem);
+// console.log(lorem.split(" "));
+// console.log(lorem.toUpperCase());
+// console.log(lorem.replace("lorem", "Mircea"));
+// console.log(lorem.substr(0, 10));
+
+
+
