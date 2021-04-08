6cdc31806d81c5c5ea527419bea91b8e8bd1049c
mirceaagr <mircea.agrigoroaie@gmail.com>
2021-04-08 12:04:36 +0200

    setInterval setTimeout + exercise

---
+++ 2021/course3/js/5.js
@@ -0,0 +1,12 @@
+// setTimeout(() => {console.log("Started ")}, 3000);
+
+// let interval = setInterval(() => {console.log("Polling")}, 1000)
+
+// setTimeout(()=>{clearInterval(interval)},5000)
+
+// for(let i = 0; i < 10; i++) {
+//     setTimeout(() => {console.log("test")}, i * 1000)
+// }
+
+let title = "IN211 web dev course";
+console.log(title.split(""));
