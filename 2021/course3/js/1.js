640f232cf31215d094664fac4ef0af15cd6ab7d9
mirceaagr <mircea.agrigoroaie@gmail.com>
2021-04-08 09:46:47 +0200

    Debugging

---
+++ 2021/course3/js/1.js
@@ -0,0 +1,30 @@
+console.log("FROM inside 1.js");
+let v1 = "Mircea";
+let v2 = 2;
+let v3 = false;
+let v4;
+let v5 = 7/0;
+let v6 = {name:"Mircea", age : 10};
+let v7 = ["🍌", "🦀"];
+
+console.log(v7[0]);
+console.log(typeof v1);
+console.log(typeof v2);
+console.log(typeof v3);
+console.log(typeof v4);
+//debugger;
+console.log(typeof v5);
+console.log(typeof v6);
+console.log(typeof v7);
+
+console.dir(Array);
+
+//document.write("Wrote directly to the htmla")
+//alert("CUCU")
+//let age = prompt("What's your age?");
+//console.log(age);
+
+
+
+
+//v6 = new Human();
