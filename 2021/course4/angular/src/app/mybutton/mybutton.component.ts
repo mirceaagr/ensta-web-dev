df0c1651d7da8df300bf93056a14ad279576af17
mirceaagr <mircea.agrigoroaie@gmail.com>
2021-04-12 14:08:34 +0200

    Fix broken EventEmitter import

--- 2021/course4/angular/src/app/mybutton/mybutton.component.ts
+++ 2021/course4/angular/src/app/mybutton/mybutton.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
-import { EventEmitter } from 'events';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-mybutton',
@@ -8,7 +7,7 @@ import { EventEmitter } from 'events';
 })
 export class MybuttonComponent implements OnInit {
   @Input() personName = "Mircea";
-  @Output() increment: EventEmitter = new EventEmitter();
+  @Output() increment: EventEmitter<any> = new EventEmitter();
   title = "Monseur";
   constructor() {
   }
