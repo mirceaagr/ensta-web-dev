018e1fe3f6ab94719684ad5ad9cfe06e0122437c
mirceaagr <mircea.agrigoroaie@gmail.com>
2021-04-26 10:07:25 +0200

    Add navigation

--- 2021/course5/frontend/src/app/app.component.ts
+++ 2021/course5/frontend/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'billing';
+  title = 'Ensta billing';
+
+  nav=[
+    {link:"/", label:"Home"},
+    {link:"bills", label:"Bills"},
+    {link:"clients", label:"Clients"},
+    {link:"products", label:"Products"},
+    {link:"settings", label:"Settings"},
+    {link:"login", label:"Login"},
+  ]
 }
