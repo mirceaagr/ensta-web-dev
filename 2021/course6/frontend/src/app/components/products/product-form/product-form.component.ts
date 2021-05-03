82d132f75bfd5ae9e25767ca535d97f41230de2f
mirceaagr <mircea.agrigoroaie@gmail.com>
2021-05-03 10:07:01 +0200

    Add product add form

--- 2021/course6/frontend/src/app/components/products/product-form/product-form.component.ts
+++ 2021/course6/frontend/src/app/components/products/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../product.model';
 
 @Component({
   selector: 'app-product-form',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
+  public product:IProduct
+  constructor() {
+    this.product = {
+      description:"",
+      name:"",
+      picture:"",
+      price:null
+    }
+    // show that the value in the component changes the value in the template
+    //setInterval(()=>{this.product.price = Math.floor(Math.random() * 10)}, 500)
+  }
 
-  constructor() { }
+  outputForm(form) {
+    console.log(form)
+  }
+
+  onSubmit(formValue){
+    console.log(formValue);
+  }
 
   ngOnInit(): void {
   }
